50c50f5aa952e2182ce5e6a09c0cc2a2192da560
Luís Falcão <lfalcao@cc.isel.pt>
2019-10-02 18:05:29 +0100

    Lesson 07: Moving the file system monitoring functionality to a module (monitoring-fs-module.js). This module returns a function object that returns an EventEmitter object

--- aula07/monitoring-fs-app.js
+++ aula07/monitoring-fs-app.js
@@ -2,9 +2,11 @@
 let path = process.argv[2] || '.'
 
 
-const mf = require('./monitoring-fs')
+const mf = require('./monitoring-fs')(path)
 
-mf(path, processFsEvent)
+mf.on(mf.FS_EVENT, processFsEvent)
+mf.on(mf.FS_EVENT, processFsEvent1)
+
 
 
 function processFsEvent(err, e) {
@@ -12,4 +14,8 @@ function processFsEvent(err, e) {
         return console.err(err)
     }
     console.log(`Event ${e.event} on file ${e.file}`)
+}
+
+function processFsEvent1(err, e) {
+    console.log(`I really don't care about the event details`)
 }
